a8b0b4bb7324899fbb0f47e2e09d190f9204e3e0
Andy Chen <andy.chen121@imperial.ac.uk>
2023-12-17 18:08:39 +0000

    simulator webpage

--- cube.py
+++ cube.py
@@ -1,95 +1,128 @@
 import numpy as np
+import matplotlib
 from matplotlib import pyplot as plt
+matplotlib.use("agg")
 import time
 
 class Cube:
-    def __init__(self, dim, colors="rybmcg"):
-        self.dim = dim
-        self.axname = "zxy"
-        self.faces = [np.array([[f for i in range(dim)] for i in range(dim)]) for f in range(6)]
+    def __init__(self, colors="rybmcg"):
+        axn = "zxy"
+        self.axname = axn
         self.colors = colors
+        self.dim = 0
+        self.axmap = {axn[0]: [0,5], axn[1]: [1,3], axn[2]: [2,4]}
+        u = (1,np.sqrt(3),2)
+        self.unit = u
+        rd = np.array([u[1], -u[0]])
+        ld = np.array([-u[1], -u[0]])
+        d = np.array([0,-u[2]])
+        self.basis1 = [(rd,ld), (rd,d), (-ld, d)]
+        self.basis2 = [self.basis1[i] for i in [2,1,0]]
+        self.rec = False
+        self.maxrec = 10
+    
+    def clearRecord(self):
+        self.recent = [[] for i in range(4)]
+        self.recwindow = []
+        f = open("static/moves.csv","w")
+        f.write("")
+        f.close()
+
+    def create(self, dim):
+        self.clearRecord()
+        self.dim = dim
+        self.faces = [np.array([[f for i in range(dim)] for i in range(dim)]) for f in range(6)]
         if self.dim <= 10:
             self.marker = "o"
         else:
             self.marker = '.'
-        self.axmap = {0: [0,5], 1: [1,3], 2: [2,4]}
-        self.unit = (1,np.sqrt(3),2)
         u = self.unit
-        rd = np.array([u[1], -u[0]])
-        ld = np.array([-u[1], -u[0]])
-        d = np.array([0,-u[2]])
         self.origs1 = [np.array([self.dim*u[1], 2*self.dim*u[2]+u[0]]), 
                       np.array([0, 1.5*self.dim*u[2]]), np.array([self.dim*u[1]+u[1], self.dim*u[2]+u[0]])]
         sx = self.dim*u[2]*2.5
         shift = [np.array([sx+u[1]/2,0]), np.array([sx,u[2]]), np.array([sx-u[2],0])]
         self.origs2 = [self.origs1[i]+shift[i] for i in [1,0,2]]
-        self.basis1 = [(rd,ld), (rd,d), (-ld, d)]
-        self.basis2 = [self.basis1[i] for i in [2,1,0]]
 
     def randomize(self, rec):
         print("randomize")
-        f = open("static/moves.csv", "w")
+        self.clearRecord()
+        tmp = self.rec
+        self.rec = rec
         for ite in range(max(pow(self.dim, 3), 20)):
             ax = int(3*np.random.random())
             loc = int(self.dim*np.random.random())
-            dir = int(2*np.random.random())
-            self.rotate(ax, loc, loc, dir)
-            if rec:
-                f.write(str(ax)+","+str(loc)+","+str(loc)+","+str(dir)+"\n")
-        f.close()
+            drn = 'L'
+            if 2*np.random.random() < 1:
+                drn = 'R'
+            self.rotate(self.axname[ax], loc, loc, drn)
+        self.rec = tmp
 
-    def rotate(self, ax, locf, loct, dir): # counterclockwise dir is true
+    def rotate(self, ax, locf, loct, drn): # counterclockwise drn is true
         if loct < locf:
             tmp = locf
             locf = loct
             loct = tmp
         if locf < 0 or loct >= self.dim:
             return
-        match ax:
-            case 0:
-                fs = [1,4,3,2]
-                if not dir:
-                    fs = [2,3,4,1]
-                line = self.faces[fs[0]][locf:loct+1,:].copy()
-                for i in range(len(fs)-1):
-                    self.faces[fs[i]][locf:loct+1,:] = self.faces[fs[i+1]][locf:loct+1,:]
-                self.faces[fs[-1]][locf:loct+1,:] = line
-            case 1:
-                if dir:
-                    line = self.faces[0][self.dim-loct-1:self.dim-locf,:].copy()
-                    self.faces[0][self.dim-loct-1:self.dim-locf,:] = self.faces[2][:,locf:loct+1].T
-                    self.faces[2][:,locf:loct+1] = self.faces[5][::-1,self.dim-loct-1:self.dim-locf]
-                    self.faces[5][:,self.dim-loct-1:self.dim-locf] = self.faces[4][:,self.dim-loct-1:self.dim-locf]
-                    self.faces[4][:,self.dim-loct-1:self.dim-locf] = line.T
-                else:
-                    line = self.faces[4][:,self.dim-loct-1:self.dim-locf].copy()
-                    self.faces[4][:,self.dim-loct-1:self.dim-locf] = self.faces[5][:,self.dim-loct-1:self.dim-locf]
-                    self.faces[5][:,self.dim-loct-1:self.dim-locf] = self.faces[2][::-1,locf:loct+1]
-                    self.faces[2][:,locf:loct+1] = self.faces[0][self.dim-loct-1:self.dim-locf,:].T
-                    self.faces[0][self.dim-loct-1:self.dim-locf,:] = line.T
-            case 2:
-                if dir:
-                    line = self.faces[0][:,self.dim-loct-1:self.dim-locf].copy()
-                    self.faces[0][:,self.dim-loct-1:self.dim-locf] = self.faces[3][::-1,locf:loct+1]
-                    self.faces[3][:,locf:loct+1] = self.faces[5][self.dim-loct-1:self.dim-locf,::-1].T
-                    self.faces[5][self.dim-loct-1:self.dim-locf,:] = self.faces[1][::-1,self.dim-loct-1:self.dim-locf].T
-                    self.faces[1][:,self.dim-loct-1:self.dim-locf] = line
-                else:
-                    line = self.faces[1][:,self.dim-loct-1:self.dim-locf].copy()
-                    self.faces[1][:,self.dim-loct-1:self.dim-locf] = self.faces[5][self.dim-loct-1:self.dim-locf,::-1].T
-                    self.faces[5][self.dim-loct-1:self.dim-locf,:] = self.faces[3][::-1,locf:loct+1].T
-                    self.faces[3][:,locf:loct+1] = self.faces[0][::-1,self.dim-loct-1:self.dim-locf]
-                    self.faces[0][:,self.dim-loct-1:self.dim-locf] = line
+        if self.rec:
+            step = [ax,locf,loct,drn]
+            for i in range(4):
+                self.recent[i] = [step[i]] + self.recent[i]
+            if not self.recwindow:
+                self.recwindow = [1]
+            else:
+                self.recwindow = [self.recwindow[0]+1] + self.recwindow
+            if len(self.recent[0]) > self.maxrec:
+                for i in range(4):   
+                    self.recent[i].pop()
+                self.recwindow.pop()
+            f = open("static/moves.csv", "a")
+            f.write(ax+","+str(locf)+","+str(loct)+","+drn+"\n")
+            f.close()
+        if self.axname[0] == ax:
+            fs = [1,4,3,2]
+            if drn == 'R':
+                fs = [2,3,4,1]
+            line = self.faces[fs[0]][locf:loct+1,:].copy()
+            for i in range(len(fs)-1):
+                self.faces[fs[i]][locf:loct+1,:] = self.faces[fs[i+1]][locf:loct+1,:]
+            self.faces[fs[-1]][locf:loct+1,:] = line
+        elif self.axname[1] == ax:
+            if drn == 'L':
+                line = self.faces[0][self.dim-loct-1:self.dim-locf,:].copy()
+                self.faces[0][self.dim-loct-1:self.dim-locf,:] = self.faces[2][:,locf:loct+1].T
+                self.faces[2][:,locf:loct+1] = self.faces[5][::-1,self.dim-loct-1:self.dim-locf]
+                self.faces[5][:,self.dim-loct-1:self.dim-locf] = self.faces[4][:,self.dim-loct-1:self.dim-locf]
+                self.faces[4][:,self.dim-loct-1:self.dim-locf] = line.T
+            else:
+                line = self.faces[4][:,self.dim-loct-1:self.dim-locf].copy()
+                self.faces[4][:,self.dim-loct-1:self.dim-locf] = self.faces[5][:,self.dim-loct-1:self.dim-locf]
+                self.faces[5][:,self.dim-loct-1:self.dim-locf] = self.faces[2][::-1,locf:loct+1]
+                self.faces[2][:,locf:loct+1] = self.faces[0][self.dim-loct-1:self.dim-locf,:].T
+                self.faces[0][self.dim-loct-1:self.dim-locf,:] = line.T
+        elif self.axname[2] == ax:
+            if drn == 'L':
+                line = self.faces[0][:,self.dim-loct-1:self.dim-locf].copy()
+                self.faces[0][:,self.dim-loct-1:self.dim-locf] = self.faces[3][::-1,locf:loct+1]
+                self.faces[3][:,locf:loct+1] = self.faces[5][self.dim-loct-1:self.dim-locf,::-1].T
+                self.faces[5][self.dim-loct-1:self.dim-locf,:] = self.faces[1][::-1,self.dim-loct-1:self.dim-locf].T
+                self.faces[1][:,self.dim-loct-1:self.dim-locf] = line
+            else:
+                line = self.faces[1][:,self.dim-loct-1:self.dim-locf].copy()
+                self.faces[1][:,self.dim-loct-1:self.dim-locf] = self.faces[5][self.dim-loct-1:self.dim-locf,::-1].T
+                self.faces[5][self.dim-loct-1:self.dim-locf,:] = self.faces[3][::-1,locf:loct+1].T
+                self.faces[3][:,locf:loct+1] = self.faces[0][::-1,self.dim-loct-1:self.dim-locf]
+                self.faces[0][:,self.dim-loct-1:self.dim-locf] = line
         fp = self.axmap[ax]
         if loct == self.dim-1:
             f = fp[1]
-            if dir:
+            if drn == 'L':
                 self.cw(f)
             else:
                 self.ccw(f)
         if locf == 0:
             f = fp[0]
-            if dir:
+            if drn == 'L':
                 self.ccw(f)
             else:
                 self.cw(f)
@@ -104,17 +137,19 @@ class Cube:
                     f.close()
                     return
                 line = step.split(",")
-                self.rotate(int(line[0]), int(line[1]), int(line[2]), int(line[3][0]))
+                self.rotate(line[0], int(line[1]), int(line[2]), line[3][0])
                 if vis:
+                    time.sleep(0.5)
                     self.visual()
         steps = f.readlines()[::-1]
         for step in steps:
             line = step.split(",")
-            dir = 0
+            drn = 'R'
             if line[3][0] == '0':
-                dir = 1
-            self.rotate(int(line[0]), int(line[1]), int(line[2]), dir)
+                drn = 'L'
+            self.rotate(line[0], int(line[1]), int(line[2]), drn)
             if vis:
+                time.sleep(0.5)
                 self.visual()
         f.close()
 
@@ -133,8 +168,9 @@ class Cube:
         self.faces[f] = newf
 
     def visual(self):
-        time.sleep(0.2)
-        plt.title("Cubic view")
+        # time.sleep(0.05)
+        plt.xticks([])
+        plt.yticks([])
         csq = []
         xs = []
         ys = []
@@ -165,12 +201,12 @@ class Cube:
         # plt.plot([x2, x2], [y2+u[0]/2, y2-u[0]-(self.dim-1)*u[2]], c='k')
         # plt.plot([x5, x5], [y2+u[0]/2, y2-u[0]-(self.dim-1)*u[2]], c='k')
         plt.savefig("static/visual.png")
+        plt.clf()
 
-c = Cube(3)
-# c.randomize(True)
-# c.moveSteps(False, False)
-c.visual()
-# for t in range(100):
-#     c.rotate(2,0,2,False)
-#     c.visual()
+# c = Cube()
+# c.create(3)
+# # c.randomize(True)
+# # c.moveSteps(False, False)
+# c.visual()
+
 
--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,54 @@
 from flask import Flask, render_template, request
+from cube import Cube
 
 app = Flask(__name__)
+sim = Cube()
 
 @app.route('/', methods=['GET', 'POST'])    # main page
 def index():
-    return render_template('index.html')
+    print("request:",request.form)
+    d = request.form.get("dimension")
+    create = request.form.get("create")
+    if create and d:
+        sim.create(int(d))
+    elif sim.dim == 0:
+        sim.create(3)
+    if request.form.get("init"):
+        sim.create(sim.dim)
+    elif request.form.get("random"):
+        sim.randomize(False)
+    if request.form.get("rec"):
+        sim.rec = True
+    elif request.form.get("recstop"):
+        sim.rec = False
+    step = request.form.get("rotate")
+    if step:
+        ax = step[0]
+        loc = int(step[1:-1])
+        drn = step[-1]
+        sim.rotate(ax,loc,loc,drn)
+    ax = request.form.get("axis")
+    locs = []
+    for i in range(sim.dim):
+        if request.form.get("loc"+str(i)):
+            locs.append(i)
+        if len(locs) >= 3:
+            break
+    drn = ''
+    if request.form.get("rsectl"):
+        drn = 'L'
+    elif request.form.get("rsectr"):
+        drn = 'R'
+    if ax and drn:
+        if len(locs) == 1:
+            loc = locs[0]
+            sim.rotate(ax, int(loc), int(loc), drn)
+        elif len(locs) == 2:
+            sim.rotate(ax, int(locs[0]), int(locs[1]), drn)
+    sim.visual()
+    rec = [[sim.recwindow[i]]+[sim.recent[j][i] for j in range(4)] for i in range(len(sim.recwindow))]
+    print(rec)
+    return render_template('index.html', cube=sim, pos=[i for i in range(sim.dim)], recent=rec)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
